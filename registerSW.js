c1e3c6cad43d396d251d454188a21921b229bb16
Damian Mora <damiancho.85@gmail.com>
2026-08-22 06:46:18 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ProtexHome/sw.js', { scope: '/ProtexHome/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
