ad653672f5e4f10a525474b818d25f219fb0b676
Damian Mora <damiancho.85@gmail.com>
2026-09-21 19:58:34 -0500

    Updates

--- assets/doorViewController-B4l07uaF.js
+++ assets/doorViewController-B4l07uaF.js
@@ -1,4 +1,4 @@
-import{D as j}from"./deviceController-CL3DpSzm.js";import{g as D,r as p,a as C,o as W,s as m,b as _,c as G,u as Z,d as Q,e as z,f as Y}from"./index-DaFHD3No.js";const J=`\r
+import{D as j}from"./deviceController-FwfYWA8P.js";import{g as D,r as p,a as C,o as W,s as m,b as _,c as G,u as Z,d as Q,e as z,f as Y}from"./index-B5xL88yU.js";const J=`\r
  <!-- Vista Puerta Motorizada -->\r
 <div class="device-container fade-in">\r
 \r
